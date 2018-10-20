189a6918ad9b3b9fdc91516555228d4eaa10eefe
JoelMekonnen <42296192+JoelMekonnen@users.noreply.github.com>
2018-10-20 17:10:10 +0300

    first change
    this is the first change

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class QuestionDb
+{
+     
+};
+int main()
+{
+    cout << "Hello world run from cmd.....\n";
+    cout << "lets see what it does.....\n";
+    system("pause");
+}
